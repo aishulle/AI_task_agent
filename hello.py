b3d33b7d345324af3ee69921fbe26d35e17c4555
Aishwarya <143599092+aishulle@users.noreply.github.com>
2025-04-14 05:48:26 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,1 @@
-def main():
-    """Prints "Hello, Aishwarya" to the console."""
-    try:
-        print("Hello, Aishwarya")
-    except Exception as e:
-        print(f"An error occurred: {e}")
 
-if __name__ == "__main__":
-    main()
